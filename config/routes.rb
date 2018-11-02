8aa0eeff124e3ac6e39b152d66601f0eb96a5de8
Steven Chen <steven.chen4hao@gmail.com>
2018-11-02 15:51:29 +0800

    Add groups controller, group model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'groups#index' #這行代表把 localhost:3000/groups 這個網址設成首頁
+  resources :groups
 end
