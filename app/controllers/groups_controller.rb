39c737cb9191db7dd7c7615d6dafb65d5aa77590
Steven Chen <steven.chen4hao@gmail.com>
2018-11-05 11:17:15 +0800

    implement group#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,6 +16,11 @@ class GroupsController < ApplicationController
 
   end
 
+  def show
+    @group = Group.find(params[:id])
+
+  end
+
   private
   def group_params
     params.require(:group).permit(:title, :description)
