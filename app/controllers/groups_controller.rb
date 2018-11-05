a78b64e73577854403451a6ac1660030d5096b7e
Steven Chen <steven.chen4hao@gmail.com>
2018-11-05 11:20:36 +0800

    implement groups#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,11 @@ class GroupsController < ApplicationController
 
   end
 
+  def edit
+    @group = Group.find(params[:id])
+
+  end
+
   private
   def group_params
     params.require(:group).permit(:title, :description)
